dffc8d6cd760b838eb6436fd446bb7426640af92
1228267639 <1228267639@qq.com>
2018-09-25 19:44:57 +0800

    公司
    检测更新

--- PanoramicDownload/Program.cs
+++ PanoramicDownload/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoUpdateHelper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,6 +15,20 @@ namespace PanoramicDownload
         [STAThread]
         static void Main()
         {
+
+            try
+            {
+                if (AutoUpdate.CheckAndUpdate())
+                {
+                    Environment.Exit(0);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("网络异常~~~赶紧检查一下吧……");
+                Environment.Exit(Environment.ExitCode);
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
